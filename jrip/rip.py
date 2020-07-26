1ddafe3169ba98ff62e1cbe813e2bf7400a24fea
Jason J. Gullickson <mr@jasongullickson.com>
2020-07-26 16:28:58 -0500

    Fixed metadata tagginggit status!

--- jrip/rip.py
+++ jrip/rip.py
@@ -7,6 +7,17 @@ import musicbrainzngs as mb
 
 import config
 
+# Escape the metadata to meet ffmpeg's requirements
+def ff_escape(s):
+    s.replace("=", "\=")
+    s.replace(";", "\;")
+    s.replace("#", "\#")
+    s.replace("\\", "\\\\")
+    s.replace("\n", "\\\n")
+    s.replace(" ", "\ ")
+    return s
+
+
 # Read the disc ID
 disc = discid.read()
 
@@ -33,6 +44,8 @@ os.makedirs(mp3_output_dir, exist_ok=True)
 with open(f"{wav_output_dir}/metadata.json", "w") as f:
     json.dump(releases, f, sort_keys=True, indent=2)
 
+# TODO: Get the album art
+
 # Process the tracks
 tracks = release["medium-list"][0]["track-list"]
 for track in tracks:
@@ -44,11 +57,10 @@ for track in tracks:
     # TODO: Add more metadata to this file
     track_metadata_filename = f"{wav_output_dir}/{track_filename}.txt"
     with open(track_metadata_filename, "w") as mf:
-        mf.write(f"title={track_title}\n")
-        mf.write(f"artist={artist}\n")
-        mf.write(f"album={title}\n")
-        # TODO: Find out if close() is needed in a with: block
-        mf.close()
+        mf.write(";FFMETADATA1\n")
+        mf.write(f"title={ff_escape(track_title)}\n")
+        mf.write(f"artist={ff_escape(artist)}\n")
+        mf.write(f"album={ff_escape(title)}\n")
 
     # Rip 
     print(f"Ripping {track_filename}")
@@ -65,9 +77,10 @@ for track in tracks:
     # For now we'll encode one track at a time, but we could do this
     # separately and in parallel if we wanted to
 
-    # TODO: Encode to FLAC
     # TODO: Encode to a dynamic number of formats defined
     # in the config file
+    # TODO: Add the album art to the encoded file
+    # TODO: Encode to FLAC
     # TODO: Suppress or re-route ffmpeg's text output
     # TODO: Don't prompt if file exists, just overwrite it
     mp3_encode_result = subprocess.run([
